39a22cb6365a57529582903619e3d87ded2ba502
Amankumar-G <amangaloliya@gmail.com>
2026-02-06 10:21:02 +0530

    feat : implement filters and errors and try testing using jest new not versioned files

---
+++ okr-server/src/parse-append-string/parse-append-string.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ParseAppendStringPipe } from './parse-append-string.pipe';
+
+describe('ParseAppendStringPipe', () => {
+  it('should be defined', () => {
+    expect(new ParseAppendStringPipe()).toBeDefined();
+  });
+});
---
+++ okr-server/src/parse-append-string/parse-append-string.pipe.ts
@@ -0,0 +1,23 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { ObjectiveDtoType } from '../objective/dto/objectiveDtoType';
+
+@Injectable()
+export class ParseAppendStringPipe implements PipeTransform {
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    if (!(value instanceof ObjectiveDtoType)) {
+      throw new BadRequestException('ObjectiveDtoType is not matched');
+    }
+
+    value = {
+      objective: `${value.objective.toString()} Welcome to incubyte`,
+    };
+    console.log(metadata);
+    console.log(value);
+    return value;
+  }
+}
