7c26263e296920b6e7332c676872d7d1f0bde093
Amankumar-G <amangaloliya@gmail.com>
2026-02-23 18:13:44 +0530

    refactor : combine coditions into single return expression

--- tdd/leap-year/leap-year.ts
+++ tdd/leap-year/leap-year.ts
@@ -1,6 +1,3 @@
 export const isLeapYear = (year: number): boolean => {
-  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  }
-  return false;
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 };
