b806eb9f3979c81425a32f2c4cfd076365fab264
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-02-24 15:22:49 +0530

    Refactor combine test cases and logic of code.

--- tdd/leap-year-2/leap-year.spec.ts
+++ tdd/leap-year-2/leap-year.spec.ts
@@ -2,13 +2,13 @@ import {describe, expect, it} from "vitest";
 import {isLeapYear} from "./leap-year";
 
 describe("isLeapYear",()=>{
-    it("should return false for a non leap year.",()=>{
+    it("should return false for a year not divisible by 4.",()=>{
         expect(isLeapYear(2019)).toBe(false);
     })
-    it("should return true for a year divisible by 4",()=>{
+    it("should return true for a year divisible by 4 but not divisible by 100",()=>{
         expect(isLeapYear(2020)).toBe(true);
     })
-    it("should return false for a year divisible by 100 .",()=>{
+    it("should return false for a year divisible by 100 and not by 400 .",()=>{
         expect(isLeapYear(1900)).toBe(false);
     })
     it("should return true for a year divisible by 400 .",()=>{
