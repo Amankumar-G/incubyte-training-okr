9153777f8b5f27b6116a621a4bb6a852a98bf448
Amankumar-G <amangaloliya@gmail.com>
2026-02-26 12:59:40 +0530

    refactor: streamline addition logic to handle multiple numbers using reduce

--- tdd/string-calculator/string-kata.ts
+++ tdd/string-calculator/string-kata.ts
@@ -1,18 +1,13 @@
 export const add = (stringInput: string): number => { 
 
-    if(stringInput.length > 0 && stringInput.includes(',')) {
-        const numbers = stringInput.split(',');
-        if(numbers.length === 2) {
-             return Number(numbers[0]) + Number(numbers[1]);
-        } else if(numbers.length === 3) {
-            return Number(numbers[0]) + Number(numbers[1]) + Number(numbers[2]);
-        } else {
-              return Number(numbers[0]) + Number(numbers[1]) + Number(numbers[2]) + Number(numbers[3]);
-        }
-    } 
-    if(stringInput.length > 0) {
-
-        return Number(stringInput);
+    if(stringInput.trim() === "") {
+        return 0;
     }
-    return 0;
+    
+    const stringArray = stringInput.split(',');
+
+    return stringArray.reduce((acc, curr) => {
+        return acc + parseInt(curr);
+    }, 0);
+    
 }
