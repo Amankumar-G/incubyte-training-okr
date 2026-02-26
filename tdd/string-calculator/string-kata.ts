e50b6219355bd3cb31eb9ed644ba04cbba9bf257
Amankumar-G <amangaloliya@gmail.com>
2026-02-26 12:34:25 +0530

    refactor: simplify input handling by converting non-empty string inputs to numbers

--- tdd/string-calculator/string-kata.ts
+++ tdd/string-calculator/string-kata.ts
@@ -1,6 +1,6 @@
 export const add = (stringInput: string): number => { 
-    if(stringInput==='5') return 5;
-    if(stringInput==='10') return 10;
-    if(stringInput==='15') return 15;
+    if(stringInput.length > 0){
+        return Number(stringInput);
+    }
     return 0;
 }
