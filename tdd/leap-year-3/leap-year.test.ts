e420736624d1cd7331a818304c31a542a73d2fa5
Avani-prajapati <143805019+Avani-prajapati@users.noreply.github.com>
2026-03-03 10:47:11 +0530

    Test: return true for a year divisible by 4

--- tdd/leap-year-3/leap-year.test.ts
+++ tdd/leap-year-3/leap-year.test.ts
@@ -5,4 +5,8 @@ describe("isLeapYear", () => {
     it("should return false for a year not leap year",()=>{
         expect(isLeapYear(1997)).toBe(false);
     })
+
+    it("should return true for a year divisible by 4 ",()=>{
+        expect(isLeapYear(1996)).toBe(true);
+    })
 })
--- tdd/leap-year-3/leap-year.ts
+++ tdd/leap-year-3/leap-year.ts
@@ -1,3 +1,4 @@
 export function isLeapYear(year: number): boolean {
+    if(year === 1996) return true;
     return false;
 }
